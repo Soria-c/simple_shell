ed70d0b51962db7976acec65a51f08931cb4572f
Carlos <soriac367@gmail.com>
2022-04-05 16:58:18 -0500

    Added more prototypes

--- main.h
+++ main.h
@@ -1,10 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/stat.h>
 
+extern char **environ;
+void printf_error(char *cmd, char *argv);
+void _execve(char **cmd);
+int _fork(int s, char *wcmd);
+char *iteration(char **delim2, char *delim, char *str, int *i, int *j, int *k);
 int strn_cmp(char *s1, char *s2, int n);
 char *str_cat(char *dest, char *src);
 char *str_cpy(char *dest, char *src);
