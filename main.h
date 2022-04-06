c82f44e2b83452942c11738d9ce85325a43ed246
Carlos <soriac367@gmail.com>
2022-04-06 14:55:18 -0500

    Added prototypes for _printf functions and some built ins

--- main.h
+++ main.h
@@ -5,9 +5,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/stat.h>
+#include <stdarg.h>
+
 
 extern char **environ;
-void printf_error(char *cmd, char *argv);
+
+int builtin_check(char **cmd, char *lin);
+void printenv(void);
+void printf_error(char *cmd, char *argv, int c);
 void _execve(char **cmd);
 int _fork(int s, char *wcmd);
 char *iteration(char **delim2, char *delim, char *str, int *i, int *j, int *k);
@@ -22,5 +27,15 @@ char *delimf(char *delim);
 int checkex2(char *s, char s2);
 int checkex(char *s, char s2, int n);
 char *str_tok(char *str, char *delim);
+int _printf(const char *format, ...);
+int fs(const char *format, char *fs, char *s, int sz, va_list a, int z, int w);
+int pc(const char *fm, char *f, char c, int x, va_list a, int o, int z, int w);
+int p(const char *fm, char *f, char *s, int x, va_list a, int o, int z, int w);
+int pv(const char *format, char *f, int sz, va_list arg, int op, int z, int w);
+void str_rev(char *s);
+void to_string(char *s, int n);
+int check(char *fs, char *s, int *op, int sz);
+int fs_init(char *s, va_list args);
+
 
 #endif /*MAIN_H*/
