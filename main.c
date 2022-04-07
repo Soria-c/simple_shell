bfdba9571a0090c0e5def005a5bc5c0d509aaa8e
enki <soriac367@gmail.com>
2022-04-07 15:15:00 -0500

    Shell now handles the following built ins: exit (with arguments) env, setenv and unsetenv

--- main.c
+++ main.c
@@ -3,7 +3,6 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/wait.h>
-#include <signal.h>
 
 /**
  * main - entry point, takes input from user.
@@ -16,8 +15,10 @@ int main(int argc __attribute__((unused)), char **argv)
 	char *lin = NULL, *wcmd, *tk, *cmd[32];
 	size_t n = 0;
 	ssize_t r;
-	int id, status, i, s, c = 0, b;
+	int id, status, i, s, c = 0, b, stnvf = 0;
 	struct stat st;
+	b_i builtins[] = {{"exit", NULL, ex_it}, {"env", printenv, NULL},
+	{"setenv", NULL, _setenv}, {"unsetenv", _unsetenv, NULL}, {NULL, NULL, NULL}};
 
 	_printf("Kali $: ");
 	for (; (r = getline(&lin, &n, stdin)) != -1; _printf("Kali $: "))
@@ -29,7 +30,7 @@ int main(int argc __attribute__((unused)), char **argv)
 		for (i = 0; tk; tk = str_tok(NULL, " "), i++)
 			cmd[i] = tk;
 		cmd[i] = NULL;
-		b = builtin_check(cmd, lin);
+		b = builtin_check(cmd, builtins, argv[0], &stnvf);
 		if (!(*cmd[0]) || !b)
 			continue;
 		s = stat(cmd[0], &st);
@@ -48,6 +49,7 @@ int main(int argc __attribute__((unused)), char **argv)
 		else if (id > 0)
 			wait(&status);
 	}
+	check_env(&stnvf);
 	free(lin);
 	return (0);
 }
