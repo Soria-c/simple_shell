708e901800f1212341a71f9bf9672fba24eb3fa6
Carlos Soria <soriac367@gmail.com>
2022-04-09 15:43:08 -0500

    Refactored

--- builtins.c
+++ builtins.c
@@ -18,10 +18,10 @@ int printenv(char **cmd UNUSED, char *argv UNUSED)
  * ex_it - implementation of the exit shell built in.
  * @cmd: commands.
  * @argv: current name of the program.
- * @stnvf: number of new values allocated in envirion.
+ * @head: address of linked list.
  * Return: 0 if it fails, otherwise exits the current shell
  */
-int ex_it(char **cmd, char *argv, int *stnvf)
+int ex_it(char **cmd, char *argv, f_s **head)
 {
 	unsigned char e;
 
@@ -35,7 +35,7 @@ int ex_it(char **cmd, char *argv, int *stnvf)
 		if (!cmd[1])
 		{
 			free(cmd[0]);
-			check_env(stnvf);
+			free_list(*head);
 			write(1, "exit\n", 5);
 			exit(0);
 		}
@@ -43,14 +43,14 @@ int ex_it(char **cmd, char *argv, int *stnvf)
 		{
 			_printf("exit\n%s: exit: %s:", argv, cmd[1]);
 			_printf(" numeric argument required\n");
-			check_env(stnvf);
+			free_list(*head);
 			free(cmd[0]);
 			exit(2);
 		}
 		e = _atoi(cmd[1]);
 		free(cmd[0]);
 		write(1, "exit\n", 5);
-		check_env(stnvf);
+		free_list(*head);
 		exit(e);
 	}
 	return (0);
@@ -60,12 +60,12 @@ int ex_it(char **cmd, char *argv, int *stnvf)
  * _setenv - sets a new environment variables or rewrites an existing one.
  * @cmd: commands.
  * @argv: current name of the program.
- * @stnvf: number of new values allocated in environ.
+ * @head: address of linked list.
  * Return: 0 always.
  */
-int _setenv(char **cmd, char *argv, int *stnvf)
+int _setenv(char **cmd, char *argv, f_s **head)
 {
-	int i, j, len1, len2, f = 1;
+	int i, j, len1, len2, lenenv;
 
 	if (array2d_len(cmd) != 3)
 	{
@@ -84,24 +84,20 @@ int _setenv(char **cmd, char *argv, int *stnvf)
 	{
 		if (!(strn_cmp(cmd[1], environ[i], len1)))
 		{
-			if (environ[i][str_len(environ[i]) + 1] == 'f')
-				f = 0;
-			for (j = 0; environ[i][j] != '='; j++)
-				;
-			environ[i][j + 1] = '\0';
-			str_cat(environ[i], cmd[2]);
-			if (!f)
-				environ[i][len1 + len2 + 2] = 'f';
+			lenenv = str_len(environ[i]);
+			if (lenenv < len1 + len2 + 1)
+				setenv_help(&environ[i], len1, len2, cmd, head);
+			else
+			{
+				for (j = 0; environ[i][j] != '='; j++)
+					;
+				environ[i][j + 1] = '\0';
+				str_cat(environ[i], cmd[2]);
+			}
 			return (0);
 		}
 	}
-	environ[i] = malloc(sizeof(char) * 1024 * 8);
-	*stnvf = *stnvf + 1;
-	str_cpy(environ[i], cmd[1]);
-	environ[i][len1] = '=';
-	environ[i][len1 + 1] = '\0';
-	str_cat(environ[i], cmd[2]);
-	environ[i][len1 + len2 + 2] = 'f';
+	setenv_help(&environ[i], len1, len2, cmd, head);
 	environ[i + 1] = NULL;
 	return (0);
 }
@@ -110,13 +106,11 @@ int _setenv(char **cmd, char *argv, int *stnvf)
  * _unsetenv - unsets and environment variable.
  * @cmd: commands.
  * @argv: current name of the program.
- * @stvnf: number of new values allocated in environ.
  * Return: 0 always.
  */
-int _unsetenv(char **cmd, char *argv __attribute__((unused)), int *stvnf)
+int _unsetenv(char **cmd, char *argv __attribute__((unused)))
 {
 	int i;
-	char *f = NULL;
 
 	if (array2d_len(cmd) != 2)
 	{
@@ -127,12 +121,7 @@ int _unsetenv(char **cmd, char *argv __attribute__((unused)), int *stvnf)
 	for (i = 0; environ[i]; i++)
 	{
 		if (!(strn_cmp(cmd[1], environ[i], str_len(cmd[1]))))
-		{
-			if (environ[i][str_len(environ[i]) + 1] == 'f')
-				f = environ[i];
-			environ[i] = "";
 			break;
-		}
 	}
 	if (!environ[i])
 	{
@@ -141,10 +130,56 @@ int _unsetenv(char **cmd, char *argv __attribute__((unused)), int *stvnf)
 	}
 	for (; environ[i]; i++)
 		environ[i] = environ[i + 1];
-	if (f)
+	return (0);
+}
+
+/**
+ * _cd - implementation of the cd shell built in.
+ * @cmd: commands.
+ * @argv: current name of the program.
+ * @head: address of linked list.i
+ * Return: 0 always.
+ */
+int _cd(char **cmd, char *argv, f_s **head)
+{
+	char *c_dir = NULL, *p_dir = NULL, *cdcmd[4];
+
+	cdcmd[0] = cmd[0];
+	cdcmd[3] = NULL;
+	if (array2d_len(cmd) > 2)
 	{
-		*stvnf = *stvnf - 1;
-		free(f);
+		_printf("%s: cd: too many arguments\n", argv);
+		_printf("Usage: cd [DIRECTORY]\n");
+		return (0);
 	}
+	if (cmd[1] && str_len(cmd[1]) == 1 && cmd[1][0] == '-')
+	{
+		cd_set(cdcmd, c_dir, p_dir, argv, head, 0);
+		return (0);
+	}
+	p_dir = getcwd(NULL, 0);
+	if (cmd[1] && chdir(cmd[1]))
+	{
+		_printf("%s: cd: %s: No such file or directory\n", argv, cmd[1]);
+		free(p_dir);
+		return (0);
+	}
+	else
+	{
+		cdcmd[1] = "OLDPWD";
+		cdcmd[2] = p_dir;
+		_setenv(cdcmd, argv, head);
+	}
+	if (!cmd[1])
+	{
+		cd_set(cdcmd, c_dir, p_dir, argv, head, 1);
+		return (0);
+	}
+	c_dir = getcwd(NULL, 0);
+	cdcmd[1] = "PWD";
+	cdcmd[2] = c_dir;
+	_setenv(cdcmd, argv, head);
+	free(c_dir);
+	free(p_dir);
 	return (0);
 }
