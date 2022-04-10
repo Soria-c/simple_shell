46c15c108dc381d9d3f012df5dc4cbafacfece1e
Carlos Soria <soriac367@gmail.com>
2022-04-10 12:35:45 -0500

    Reworked and refactored, program now handles ';' separator

--- main.c
+++ main.c
@@ -8,14 +8,17 @@
  */
 int main(int argc __attribute__((unused)), char **argv)
 {
-	char *lin = NULL, *wcmd = NULL, *tk = NULL, *cmd[256];
+	char *lin = NULL;
 	size_t n = 0;
 	f_s *head = NULL;
-	int id, i, s = 1, c = 0, r;
-	struct stat st;
-	b_i builtins[] = {{"exit", NULL, ex_it}, {"env", printenv, NULL},
-	{"setenv", NULL, _setenv}, {"unsetenv", _unsetenv, NULL},
-	{"cd", NULL, _cd}, {NULL, NULL, NULL}};
+	int c = 0, r;
+	cmds *cm, *fcm;
+	b_i builtins[] = {{"exit", NULL, NULL, ex_it},
+			{"env", printenv, NULL, NULL},
+			{"setenv", NULL, _setenv, NULL},
+			{"unsetenv", _unsetenv, NULL, NULL},
+			{"cd", NULL, _cd, NULL},
+			{NULL, NULL, NULL, NULL}};
 
 	signal(SIGINT, handler);
 	for (_prompt(); (r = getline(&lin, &n, stdin)) != -1; _prompt())
@@ -23,30 +26,15 @@ int main(int argc __attribute__((unused)), char **argv)
 		c++;
 		if (lin[r - 1] == '\n')
 			lin[r - 1] = '\0';
-		tk = str_tok(lin, " \t");
-		for (i = 0; tk; tk = str_tok(NULL, " \t"), i++)
-			cmd[i] = tk;
-		cmd[i] = NULL;
-		if (!(*cmd[0]) || !builtin_check(cmd, builtins, argv[0], &head))
-			continue;
-		s = stat(cmd[0], &st);
-		if (s)
-			wcmd = _which(cmd[0]);
-		if ((!s) || (s && wcmd))
-			id = _fork(s, wcmd);
-		else
-		{
-			free(wcmd);
-			printf_error(cmd[0], argv[0], c);
-			continue;
-		}
-		if (!id)
-			_execve(cmd, argv[0], c);
-		else if (id > 0)
-			wait(NULL);
+		fcm = cm = command_builder(lin);
+		for (; cm; cm = cm->next)
+			exec_all(cm, c, argv[0], &head, builtins);
+		free_list(NULL, fcm);
 	}
-	free_list(head);
+	free_list(head, NULL);
 	free(lin);
 	return (0);
 }
+
+
 
